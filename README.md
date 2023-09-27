b4a2a3a8c1aba6821eb9cc4de00a277a2e4783de
nchinling <nchinling@gmail.com>
2023-09-27 19:22:55 +0800

    free response

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Write up
 
-# Angular Materials
-# Angular 
-# Spring Boot
-# SQL, MongoDB
+## Angular Materials
+## Angular 
+## Spring Boot
+## SQL, MongoDB
 
-# 22 Sep
+## 22 Sep
 - Used Angular Material for styling
 - Created registration
 
-# 25 Sep
+## 25 Sep
 - Created question form and question model
 
-# 27 Sep
+## 27 Sep
 - created working form with ability to add new questions
+- attempt to create free response
--- lms-frontend/src/app/components/quiz.component.ts
+++ lms-frontend/src/app/components/quiz.component.ts
@@ -16,18 +16,20 @@ import { AccountService } from '../services/account.service';
     quizForm!: FormGroup
     optionForm!: FormGroup
     questionFormGroup!: FormGroup
-
+   
 
     fb = inject(FormBuilder)
     router = inject(Router)
     accountSvc = inject(AccountService)
+
+    currentQuestionType: 'MCQ' | 'FreeResponse' = 'MCQ';
 
     title = 'FormArray Example in Angular Reactive forms';
 
     constructor() {
    
     this.quizForm = this.fb.group({
-        name: '',
+        title: '',
         questions: this.fb.array([]) ,
         
       });
@@ -43,13 +45,20 @@ import { AccountService } from '../services/account.service';
     }
 
 
-    createNewQuestion(): FormGroup {
+    createNewMCQ(): FormGroup {
       return this.fb.group({
         question: this.fb.control<string>('What is 1+1', [Validators.required]),
         option1: this.fb.control<string>('2', [Validators.required]),
         option2: this.fb.control<string>('3', [Validators.required]),
         option3: this.fb.control<string>('4', [Validators.required]),
         option4: this.fb.control<string>('5', [Validators.required]),
+        answer: this.fb.control<string>('', [Validators.required]),
+      })
+    }
+
+    createNewFreeResponse(): FormGroup {
+      return this.fb.group({
+        question: this.fb.control<string>('What is 1+1', [Validators.required]),
         answer: this.fb.control<string>('', [Validators.required]),
       })
     }
@@ -61,8 +70,14 @@ import { AccountService } from '../services/account.service';
     }
 
 
-    addQuestion() {
-      this.questions.push(this.createNewQuestion());
+    addMCQ() {
+      this.currentQuestionType = 'MCQ';
+      this.questions.push(this.createNewMCQ());
+    }
+
+    addFreeResponse() {
+      this.currentQuestionType = 'FreeResponse';
+      this.questions.push(this.createNewFreeResponse());
     }
 
     removeQuestion(i:number) {
@@ -77,10 +92,17 @@ import { AccountService } from '../services/account.service';
       this.options.removeAt(i);
     }
 
-  
+
+    formMode: 'MCQ' | 'FreeResponse' = 'MCQ'; // Initial mode is MCQ
+
+    toggleMode(mode: 'MCQ' | 'FreeResponse') {
+      this.formMode = mode;
+    }
+
     processQuiz() {
       console.log(this.quizForm.value);
     }
+
    
   }
    
